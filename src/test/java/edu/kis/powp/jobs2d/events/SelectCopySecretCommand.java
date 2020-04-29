2c5eb92ae631d2f168940545fd4be493d2efabf9
Hergoln <juliusz.malka@o2.pl>
2020-04-29 15:38:47 +0200

    Updated SelectCopySecretCommand testing event

--- src/test/java/edu/kis/powp/jobs2d/events/SelectCopySecretCommand.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectCopySecretCommand.java
@@ -1,6 +1,5 @@
 package edu.kis.powp.jobs2d.events;
 
-import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.command.*;
 import edu.kis.powp.jobs2d.command.manager.DriverCommandManager;
 import edu.kis.powp.jobs2d.features.CommandsFeature;
@@ -8,73 +7,65 @@ import edu.kis.powp.jobs2d.features.CommandsFeature;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SelectCopySecretCommand implements ActionListener {
-
-	private static ICompoundCommand compound = new ICompoundCommand() {
-
-		List<DriverCommand> driverCommands = null;
-
-		@Override
-		public void execute(Job2dDriver driver) {
-			driverCommands.forEach((c) -> c.execute(driver));
-		}
-
-		@Override
-		public Iterator<DriverCommand> iterator() {
-			return driverCommands.iterator();
-		}
-
-		@Override
-		public DriverCommand clone() throws CloneNotSupportedException {
-			return (DriverCommand) super.clone();
-		}
-
-		@Override
-		public void setCommands(List<DriverCommand> driverCommands) {
-			this.driverCommands = driverCommands;
-		}
-	};
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
-		DriverCommand first, second, third; // test DriverCommands
-
+		DefaultCompoundCommand compound = null;
 		List<DriverCommand> commands = new ArrayList<DriverCommand>();
-		commands.add(new SetPositionCommand(-20, -50));
-		commands.add(new OperateToCommand(-20, -50));
-		commands.add(new SetPositionCommand(-20, -40));
-		compound.setCommands(commands);
-		first = CommandCopier.copyCommand(compound);
+		DriverCommand first, second, third, lastOne; // test DriverCommands
 
-		commands.clear();
-		commands.add(new OperateToCommand(-20, 50));
-		commands.add(new SetPositionCommand(0, -50));
-		commands.add(new OperateToCommand(0, -50));
-		commands.add(new SetPositionCommand(0, -40));
-		commands.add(new OperateToCommand(0, 50));
-		compound.setCommands(commands);
-		second = CommandCopier.copyCommand(compound);
+		try {
+			commands.add(new SetPositionCommand(-20, -50));
+			commands.add(new OperateToCommand(-20, -50));
+			commands.add(new SetPositionCommand(-20, -40));
+			compound = new DefaultCompoundCommand(commands);
+			first = CommandsFeature.deepCopyCommand(compound);
 
-		commands.clear();
-		commands.add(new SetPositionCommand(70, -50));
-		commands.add(new OperateToCommand(20, -50));
-		commands.add(new OperateToCommand(20, 0));
-		commands.add(new OperateToCommand(70, 0));
-		commands.add(new OperateToCommand(70, 50));
-		commands.add(new OperateToCommand(20, 50));
-		compound.setCommands(commands);
-		third = CommandCopier.copyCommand(compound);
+			commands.clear();
+			commands.add(new OperateToCommand(-20, 50));
+			commands.add(new SetPositionCommand(0, -50));
+			commands.add(new OperateToCommand(0, -50));
+			commands.add(new SetPositionCommand(0, -40));
+			commands.add(new OperateToCommand(0, 50));
+			compound = new DefaultCompoundCommand(commands);
+			second = CommandsFeature.deepCopyCommand(compound);
 
-		List<DriverCommand> toCopy = new ArrayList<>();
-		toCopy.add(first);
-		toCopy.add(second);
-		toCopy.add(third);
+			commands.clear();
+			commands.add(new SetPositionCommand(70, -50));
+			commands.add(new OperateToCommand(20, -50));
+			commands.add(new OperateToCommand(20, 0));
+			commands.add(new OperateToCommand(70, 0));
+			commands.add(new OperateToCommand(70, 50));
+			commands.add(new OperateToCommand(20, 50));
+			compound = new DefaultCompoundCommand(commands);
+			third = CommandsFeature.deepCopyCommand(compound);
+
+			commands.clear();
+			commands.add(first);
+			commands.add(second);
+			compound = new DefaultCompoundCommand(commands);
+			first = CommandsFeature.deepCopyCommand(compound);
+
+			commands.clear();
+			commands.add(first);
+			commands.add(third);
+			compound = new DefaultCompoundCommand(commands);
+			lastOne = CommandsFeature.deepCopyCommand(compound);
+
+			commands.clear();
+			commands.add(lastOne);
+		} catch (CloneNotSupportedException exc) {
+			Logger.getLogger("global").log(Level.WARNING, exc.toString());
+			return;
+		}
 
 		DriverCommandManager manager = CommandsFeature.getDriverCommandManager();
-		manager.setCurrentCommand(toCopy, "CopiedTopSecretCommand");
+		manager.setCurrentCommand(commands, "CopiedTopSecretCommand");
 	}
 }
