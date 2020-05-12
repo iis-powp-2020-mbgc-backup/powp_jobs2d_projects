287016885b6e406103ac54af1859a382f3653452
Przemysław Frąg <215719@edu.p.lodz.pl>
2020-05-12 16:55:54 +0200

    Added ImmutableComplexCommand class

---
+++ src/main/java/edu/kis/powp/jobs2d/command/ImmutableComplexCommand.java
@@ -0,0 +1,33 @@
+package edu.kis.powp.jobs2d.command;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class ImmutableComplexCommand implements ICompoundCommand {
+    private final List<DriverCommand> driverCommands;
+    
+    public ImmutableComplexCommand(List<DriverCommand> commands) {
+        driverCommands = Collections.unmodifiableList(new ArrayList<DriverCommand>(commands));
+    }
+    
+    public ImmutableComplexCommand(DriverCommand ... commands) {
+        this(Arrays.asList(commands));
+    }
+    
+    @Override
+    public void execute(Job2dDriver driver) {
+        for (DriverCommand command : driverCommands) {
+            command.execute(driver);
+        }
+    }
+    
+    @Override
+    public Iterator<DriverCommand> iterator() {
+        return driverCommands.iterator();
+    }
+}
