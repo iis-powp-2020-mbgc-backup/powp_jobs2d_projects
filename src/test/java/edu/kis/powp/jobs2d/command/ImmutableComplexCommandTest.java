46a024fbbb111f6b80a717b73bc507e23709aa82
Przemysław Frąg <215719@edu.p.lodz.pl>
2020-05-13 12:29:38 +0200

    Corrected ImmutableComplexCommand test

--- src/test/java/edu/kis/powp/jobs2d/command/ImmutableComplexCommandTest.java
+++ src/test/java/edu/kis/powp/jobs2d/command/ImmutableComplexCommandTest.java
@@ -5,39 +5,28 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import org.junit.Assert;
-import java.util.concurrent.atomic.AtomicInteger;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ImmutableComplexCommandTest {
 
     @Test
-    void iterator() {
-
-        List<DriverCommand> testList = new ArrayList<>();
-        SetPositionCommand positionCommand = new SetPositionCommand(50, 50);
-        testList.add(positionCommand);
-        testList.add(new OperateToCommand(50, 50));
-        testList.add(new SetPositionCommand(50, 50));
-        testList.add(new OperateToCommand(50, 50));
-        testList.add(new SetPositionCommand(50, 50));
-        testList.add(new OperateToCommand(50, 50));
-
-
-        ImmutableComplexCommand complexCommand = new ImmutableComplexCommand(testList);
-        positionCommand = new SetPositionCommand(100, 100);
-        testList.add(positionCommand);
-
-       Iterator<DriverCommand> CommandIterator = complexCommand.iterator();
-        int checkValue = 0;
-          while(CommandIterator.hasNext()){
-            DriverCommand nextIterator = CommandIterator.next();
-            checkValue++;
-            Exception exception = assertThrows(UnsupportedOperationException.class, CommandIterator::remove);
+    void immutability() {
+        List<DriverCommand> srcList = new ArrayList<>();
+        srcList.add(new SetPositionCommand(10, 10));
+        srcList.add(new SetPositionCommand(20, 20));
+        
+        List<DriverCommand> testList = new ArrayList<DriverCommand>(srcList);
+        
+        ImmutableComplexCommand immutableComplexCommand = new ImmutableComplexCommand(testList);
+        
+        testList.set(1, new OperateToCommand(30, 30));
+        
+        Iterator<DriverCommand> iterator = immutableComplexCommand.iterator();
+        
+        for (DriverCommand command : srcList) {
+            assertEquals(command, iterator.next());
+        }
+        
+        assertFalse(iterator.hasNext());
     }
-    assertEquals(checkValue, 6);
-
-}
-
 }
