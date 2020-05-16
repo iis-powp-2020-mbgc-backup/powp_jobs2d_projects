3ebb57de806ca8c3eb459a98378a2483a82d9534
Pietruszewski Mateusz <215851@edu.p.lodz.pl>
2020-05-16 16:45:21 +0200

    Created Load Macro listener

---
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectLoadMacroDriverListener.java
@@ -0,0 +1,16 @@
+package edu.kis.powp.jobs2d.events;
+
+import edu.kis.powp.jobs2d.features.CommandsFeature;
+import edu.kis.powp.jobs2d.features.DriverFeature;
+import edu.kis.powp.jobs2d.features.MacroFeature;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SelectLoadMacroDriverListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        CommandsFeature.getDriverCommandManager().setCurrentCommand(MacroFeature.getMacroDriverDecorator().getDriverCommandList(), "Macro");
+        DriverFeature.getDriverManager().setCurrentDriver(MacroFeature.getMacroDriverDecorator().getJob2dDriver());
+    }
+}
