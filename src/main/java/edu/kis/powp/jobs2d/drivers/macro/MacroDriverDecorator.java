2f9766b099adb87c0103130e211249640206c52f
AndrzejBilant <andrzej.bilant@gmail.com>
2020-05-19 10:22:22 +0200

    changed space to tabs and removed whitespace in MacroDriverDecorator.java
    Signed-off-by: AndrzejBilant <andrzej.bilant@gmail.com>

--- src/main/java/edu/kis/powp/jobs2d/drivers/macro/MacroDriverDecorator.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/macro/MacroDriverDecorator.java
@@ -8,48 +8,47 @@ import edu.kis.powp.jobs2d.command.SetPositionCommand;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class MacroDriverDecorator implements Job2dDriver {
-    private List<DriverCommand> driverCommandList;
+	private List<DriverCommand> driverCommandList;
 
-    private Job2dDriver driver;
+	private Job2dDriver driver;
 
-    public MacroDriverDecorator() {
-        this.driverCommandList = new ArrayList<>();
-    }
+	public MacroDriverDecorator() {
+		this.driverCommandList = new ArrayList<>();
+	}
 
-    public MacroDriverDecorator(List<DriverCommand> driverCommandList) {
-        this.driverCommandList = driverCommandList;
-    }
+	public MacroDriverDecorator(List<DriverCommand> driverCommandList) {
+		this.driverCommandList = driverCommandList;
+	}
 
-    public void setDriver(Job2dDriver driver) {
-        this.driver = driver;
-    }
+	public void setDriver(Job2dDriver driver) {
+		this.driver = driver;
+	}
 
-    public void clearCommandSet() {
-        driverCommandList = new ArrayList<>();
-    }
+	public void clearCommandSet() {
+		driverCommandList = new ArrayList<>();
+	}
 
-    public List<DriverCommand> getDriverCommandList() {
-        return driverCommandList;
-    }
+	public List<DriverCommand> getDriverCommandList() {
+		return driverCommandList;
+	}
 
-    public Job2dDriver getDriver() {
-        return driver;
-    }
+	public Job2dDriver getDriver() {
+		return driver;
+	}
 
-    @Override
-    public void setPosition(int x, int y) {
-        driverCommandList.add(new SetPositionCommand(x, y));
-    }
+	@Override
+	public void setPosition(int x, int y) {
+		driverCommandList.add(new SetPositionCommand(x, y));
+	}
 
-    @Override
-    public void operateTo(int x, int y) {
-        driverCommandList.add(new OperateToCommand(x, y));
-    }
+	@Override
+	public void operateTo(int x, int y) {
+		driverCommandList.add(new OperateToCommand(x, y));
+	}
 
-    @Override
-    public String toString() {
-        return "MacroDriverAdapter";
-    }
+	@Override
+	public String toString() {
+		return "MacroDriverAdapter";
+	}
 }
