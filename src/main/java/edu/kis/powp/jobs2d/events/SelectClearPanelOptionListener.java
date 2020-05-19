7335769a1c2163826055183f75f1ccc2f0207392
Chester193 <46018874+Chester193@users.noreply.github.com>
2020-05-20 00:33:11 +0200

    some code cleanup

--- src/main/java/edu/kis/powp/jobs2d/events/SelectClearPanelOptionListener.java
+++ src/main/java/edu/kis/powp/jobs2d/events/SelectClearPanelOptionListener.java
@@ -1,23 +1,25 @@
 package edu.kis.powp.jobs2d.events;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseListener;
-
-import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
 
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class SelectClearPanelOptionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		DrawerFeature.getDrawerController().clearPanel();
-		MouseListener[] listeners = DrawerFeature.getPanel().getMouseListeners();
-		for (MouseListener listener: listeners) {
-			DrawerFeature.getPanel().removeMouseListener(listener);
-		}
+		cleanMouseListeners();
+	}
+
+	private void cleanMouseListeners(){
+		Arrays.stream(DrawerFeature
+				.getPanel()
+				.getMouseListeners())
+				.forEach(listener -> DrawerFeature
+						.getPanel()
+						.removeMouseListener(listener));
 	}
 }
