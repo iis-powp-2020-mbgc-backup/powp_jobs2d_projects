5045f401d576d8510a133fae368dd3925209eab0
Barabasz.Rafal <215671@edu.p.lodz.pl>
2020-05-19 12:54:59 +0200

    Replaced spaces to tabs.

--- src/main/java/edu/kis/powp/jobs2d/features/MacroFeature.java
+++ src/main/java/edu/kis/powp/jobs2d/features/MacroFeature.java
@@ -3,9 +3,10 @@ package edu.kis.powp.jobs2d.features;
 import edu.kis.powp.jobs2d.drivers.MacroDriverDecorator;
 
 public class MacroFeature {
+
     private static MacroDriverDecorator macroDriverDecorator;
 
-    public static void setupMacroDriverDecorator(){
+    public static void setupMacroDriverDecorator() {
         macroDriverDecorator = new MacroDriverDecorator();
     }
 
