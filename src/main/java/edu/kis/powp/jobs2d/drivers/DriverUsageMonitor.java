6a5e46b557f5962aadac14517fb7e9beb847be50
Asmei <kamsykula@gmail.com>
2020-05-21 20:53:09 +0200

    Compute distance in DriverUsageMonitor

--- src/main/java/edu/kis/powp/jobs2d/drivers/DriverUsageMonitor.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/DriverUsageMonitor.java
@@ -3,6 +3,12 @@ package edu.kis.powp.jobs2d.drivers;
 import edu.kis.powp.jobs2d.Job2dDriver;
 
 public class DriverUsageMonitor extends Job2dDriverDecorator {
+	private double operateToDistance;
+	private double setPositionDistance;
+	private int previousPositionX;
+	private int previousPositionY;
+	
+	
 	public DriverUsageMonitor(Job2dDriver driver) {
 		super(driver);
 	}
@@ -10,10 +16,36 @@ public class DriverUsageMonitor extends Job2dDriverDecorator {
 	@Override
 	public void setPosition(int x, int y) {
 		super.setPosition(x, y);
+		
+		this.setPositionDistance += computeDistance(x, y);
+		
+		this.previousPositionX = x;
+		this.previousPositionY = y;
 	}
 	
 	@Override
 	public void operateTo(int x, int y) {
 		super.operateTo(x, y);
+		
+		this.operateToDistance += computeDistance(x, y);
+		
+		this.previousPositionX = x;
+		this.previousPositionY = y;
+	}
+	
+	public double getOperateToDistance() {
+		return this.operateToDistance;
+	}
+	
+	public double getTotalToDistance() {
+		return this.operateToDistance + this.setPositionDistance;
+	}
+	
+	public double getSetPositionDistance() {
+		return setPositionDistance;
+	}
+	
+	private double computeDistance(int x, int y) {
+		return Math.sqrt(Math.pow(x - previousPositionX, 2) + Math.pow(y - previousPositionY, 2));
 	}
 }
