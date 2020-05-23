6edc44fe27480ec906ad1a5a73a9b39691590944
Daniel Duczymiński <215710@edu.p.lodz.pl>
2020-05-23 15:01:56 +0200

    counting int wastage fixed + totalUsage added

--- src/main/java/edu/kis/powp/jobs2d/drivers/InkUsageDriver.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/InkUsageDriver.java
@@ -6,22 +6,28 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 import java.util.logging.Logger;
 
 public class InkUsageDriver implements Job2dDriver {
-    private int x, y;
+
+    private int x0, y0;
+    private double totalUsage;
     Logger logger = Logger.getLogger("global");
     private DrawPanelController drawController;
     private ILine line;
+
 
 
     public InkUsageDriver(ILine line, DrawPanelController drawController){
         super();
         this.drawController = drawController;
         this.line = line;
+        this.x0 = 0;
+        this.y0 = 0;
+        this.totalUsage = 0;
     }
 
     @Override
     public void setPosition(int x, int y){
-        this.x = x;
-        this.y = y;
+        this.x0 = x;
+        this.y0 = y;
     }
 
     double inkCounter(int xStart, int xEnd, int yStart, int yEnd){
@@ -31,21 +37,24 @@ public class InkUsageDriver implements Job2dDriver {
     }
 
     @Override
-    public void operateTo(int i, int i1) {
+    public void operateTo(int x1, int y1) {
 
-        line.setStartCoordinates(this.x, this.y);
-        this.setPosition(x, y);
-        line.setEndCoordinates(x, y);
+        line.setStartCoordinates(this.x0, this.y0);
+        this.setPosition(x1, y1);
+        line.setEndCoordinates(this.x0, this.y0);
 
         drawController.drawLine(line);
 
-        double waste = 0;
+        double waste;
         int xStart = line.getStartCoordinateX();
         int xEnd = line.getEndCoordinateX();
         int yStart = line.getStartCoordinateY();
         int yEnd = line.getEndCoordinateY();
 
         waste = inkCounter(xStart, xEnd, yStart, yEnd);
-        this.logger.info("Ink: " + String.format ("%.3f", waste));
+        totalUsage += waste;
+        this.logger.info("Ink wasted per move: " + String.format ("%.3f", waste) + "units");
+        this.logger.info("Total used ink: " + String.format ("%.3f", totalUsage) + "units");
+        this.logger.info("-----------------------------------");
     }
 }
