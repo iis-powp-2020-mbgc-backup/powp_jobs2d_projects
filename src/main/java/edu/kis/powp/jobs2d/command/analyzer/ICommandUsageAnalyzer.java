4ac7147f578f3b99c8cb6341e18e5e6eec0074a7
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-25 20:44:19 +0200

    Added java docs for interfaces.

--- src/main/java/edu/kis/powp/jobs2d/command/analyzer/ICommandUsageAnalyzer.java
+++ src/main/java/edu/kis/powp/jobs2d/command/analyzer/ICommandUsageAnalyzer.java
@@ -3,7 +3,19 @@ package edu.kis.powp.jobs2d.command.analyzer;
 import edu.kis.powp.jobs2d.command.DriverCommandVisitor;
 import edu.kis.powp.jobs2d.command.ICompoundCommand;
 
+/**
+ * This interface was defined for complex commands benchmarking and analysis of usage purpose.
+ */
 public interface ICommandUsageAnalyzer extends DriverCommandVisitor {
+    /**
+     * Performs command analysis based on injected policy.
+     * @param compoundCommand command to benchmark.
+     */
     void analyze(ICompoundCommand compoundCommand);
+
+    /**
+     * Sets current computation policy of analyzer.
+     * @param policy policy to set.
+     */
     void setComputationPolicy(IComputationPolicy policy);
 }
--- src/main/java/edu/kis/powp/jobs2d/command/analyzer/IComputationPolicy.java
+++ src/main/java/edu/kis/powp/jobs2d/command/analyzer/IComputationPolicy.java
@@ -1,9 +1,43 @@
 package edu.kis.powp.jobs2d.command.analyzer;
 
+/**
+ * Model of computation defined for benchmarking of attached commands or in future whole devices.
+ */
 public interface IComputationPolicy {
+    /**
+     * Performs step computation, based on provided coordinates, device properties, and type of usage
+     * @see edu.kis.powp.jobs2d.command.analyzer.UsageType
+     * @param start_x current x position of head or other device
+     * @param start_y current y position of head or other device
+     * @param end_x end x position of head or other device
+     * @param end_y end y position of head or other device
+     * @return computed factor (i.e. time) for current step.
+     */
     double compute(int start_x, int start_y, int end_x, int end_y, UsageType type);
+
+    /**
+     * Sets acceleration factor for examined device or object
+     * @param d acceleration in m/s^2.
+     */
     void setAccelerationFactor(double d);
+
+    /**
+     * Sets deceleration factor for examined device or object
+     * @param d deceleration in m/s^2.
+     */
     void setDecelerationFactor(double d);
+
+    /**
+     * Sets velocity factor for examined device or object
+     * @param d deceleration in m/s^2.
+     */
     void setVelocity(double d);
+
+    /**
+     * Sets distance unit for current examination (it determines distances between coordinates)
+     * @see #compute
+     * @param unit unit enum
+     * @see edu.kis.powp.jobs2d.command.analyzer.Unit
+     */
     void setBasicDistanceUnit(Unit unit);
 }
