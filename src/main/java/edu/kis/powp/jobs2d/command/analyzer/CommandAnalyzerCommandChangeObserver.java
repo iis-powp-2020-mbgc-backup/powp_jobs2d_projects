5e4812dddf114a2e3f95560fd7b1452940662207
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-26 10:31:01 +0200

    Implemented CommandAnalyzerCommandChangeObserver and injected it into CommandsFeature to add it to subscribers collection of DriverCommandManager

--- src/main/java/edu/kis/powp/jobs2d/command/analyzer/CommandAnalyzerCommandChangeObserver.java
+++ src/main/java/edu/kis/powp/jobs2d/command/analyzer/CommandAnalyzerCommandChangeObserver.java
@@ -1,13 +1,26 @@
 package edu.kis.powp.jobs2d.command.analyzer;
 
+import edu.kis.powp.jobs2d.command.DriverCommand;
+import edu.kis.powp.jobs2d.command.ICompoundCommand;
+import edu.kis.powp.jobs2d.features.CommandsFeature;
 import edu.kis.powp.observer.Subscriber;
 
 public class CommandAnalyzerCommandChangeObserver implements Subscriber {
 
-    @Override
-    public void update() {
+    private ICommandUsageAnalyzer analyzer;
 
+    public CommandAnalyzerCommandChangeObserver(ICommandUsageAnalyzer analyzer) {
+        super();
+        this.analyzer = analyzer;
     }
 
-    
+
+    @Override
+    public void update() {
+        DriverCommand command = CommandsFeature.getDriverCommandManager().getCurrentCommand();
+        if(command instanceof ICompoundCommand){
+            analyzer.analyze((ICompoundCommand) command);
+        }
+    }
+
 }
