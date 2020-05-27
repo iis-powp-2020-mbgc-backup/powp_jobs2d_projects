d28f80601b60acdde6bb0dcdcb34b290965f1061
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-27 18:31:33 +0200

    Moved mapUnit method to enum Unit

--- src/main/java/edu/kis/powp/jobs2d/command/analyzer/StandardComputationPolicy.java
+++ src/main/java/edu/kis/powp/jobs2d/command/analyzer/StandardComputationPolicy.java
@@ -1,5 +1,7 @@
 package edu.kis.powp.jobs2d.command.analyzer;
 
+import static edu.kis.powp.jobs2d.command.analyzer.Unit.mapUnit;
+
 public class StandardComputationPolicy implements IComputationPolicy {
 
     private final double inkFactor;
@@ -42,27 +44,6 @@ public class StandardComputationPolicy implements IComputationPolicy {
         statistics.addRecord("distance", distance);
         statistics.addRecord("totalInkUsed", totalInk);
         return statistics;
-    }
-
-    private double mapUnit(Unit distanceUnit) {
-        switch (distanceUnit) {
-            case NANOMETER:
-                return Math.pow(10, -9);
-            case MICROMETER:
-                return Math.pow(10, -6);
-            case MILLIMETER:
-                return Math.pow(10, -3);
-            case CENTIMETER:
-                return Math.pow(10, -2);
-            case DECIMETER:
-                return Math.pow(10, -1);
-            case METER:
-                return 1;
-            case KILOMETER:
-                return Math.pow(10, 3);
-            default:
-                throw new IllegalStateException("Unexpected value: " + distanceUnit);
-        }
     }
 
     public static class ComputationPolicyBuilder {
--- src/main/java/edu/kis/powp/jobs2d/command/analyzer/Unit.java
+++ src/main/java/edu/kis/powp/jobs2d/command/analyzer/Unit.java
@@ -7,5 +7,26 @@ public enum Unit {
     CENTIMETER,
     DECIMETER,
     METER,
-    KILOMETER,
+    KILOMETER;
+
+    public static double mapUnit(Unit distanceUnit) {
+        switch (distanceUnit) {
+            case NANOMETER:
+                return Math.pow(10, -9);
+            case MICROMETER:
+                return Math.pow(10, -6);
+            case MILLIMETER:
+                return Math.pow(10, -3);
+            case CENTIMETER:
+                return Math.pow(10, -2);
+            case DECIMETER:
+                return Math.pow(10, -1);
+            case METER:
+                return 1;
+            case KILOMETER:
+                return Math.pow(10, 3);
+            default:
+                throw new IllegalStateException("Unexpected value: " + distanceUnit);
+        }
+    }
 }
