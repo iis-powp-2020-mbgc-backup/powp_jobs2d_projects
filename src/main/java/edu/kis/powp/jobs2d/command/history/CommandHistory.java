e482a08877634313bf5cfa945aca98cc0234e72f
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-06-03 00:02:38 +0200

    Added class for representing whole history command entries

--- src/main/java/edu/kis/powp/jobs2d/command/history/CommandHistory.java
+++ src/main/java/edu/kis/powp/jobs2d/command/history/CommandHistory.java
@@ -1,5 +1,21 @@
 package edu.kis.powp.jobs2d.command.history;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.swing.DefaultListModel;
+
 public class CommandHistory {
 
+    static DefaultListModel<HistoryEntry> entryHistoryList = new DefaultListModel<>();
+
+    public static void addCommandEntry(String commandName) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        HistoryEntry historyEntry = new HistoryEntry(LocalTime.now().format(formatter), commandName);
+        entryHistoryList.addElement(historyEntry);
+    }
+
+    public static void clearHistory() {
+        entryHistoryList.clear();
+    }
 }
