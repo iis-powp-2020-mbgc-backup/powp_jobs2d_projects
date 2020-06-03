2abd8bd51ec2538ae3f8bd42c9781a9bd9aa41f8
Krzysztof Chybicki <krysiek97@interia.eu>
2020-06-03 21:12:43 +0200

    removed unused imports

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDriverAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDriverAdapter.java
@@ -4,9 +4,6 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.powp.jobs2d.Job2dDriver;
 
-import static java.lang.Math.pow;
-import static java.lang.Math.sqrt;
-
 /**
  * Line adapter - Job2dDriver with DrawPanelController object.
  */
