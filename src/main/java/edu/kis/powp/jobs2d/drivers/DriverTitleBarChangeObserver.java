267caf839f95fcfff711ea660682dcf9ea42786a
215877 <q89nTi10>
2020-06-15 19:26:52 +0200

    Added JavaDoc comments for DriverManager, DriverTitleBarChangeObserver, PointTransformation and transformation classes

--- src/main/java/edu/kis/powp/jobs2d/drivers/DriverTitleBarChangeObserver.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/DriverTitleBarChangeObserver.java
@@ -4,12 +4,22 @@ import edu.kis.powp.jobs2d.features.DriverFeature;
 import edu.kis.powp.observer.Subscriber;
 
 
+/**
+ * Observer for a driver change event.
+ */
 public class DriverTitleBarChangeObserver implements Subscriber {
 
+    /**
+     * Update driver info.
+     */
     public void update() {
         DriverFeature.updateDriverInfo();
     }
 
+    /**
+     * Returns a string representation of this observer.
+     * @return Name of the observer.
+     */
     public String toString() {
         return "Driver Title Bar Change Observer";
     }
--- src/main/java/edu/kis/powp/jobs2d/transformation/RotatePointTransformation.java
+++ src/main/java/edu/kis/powp/jobs2d/transformation/RotatePointTransformation.java
@@ -1,17 +1,36 @@
 package edu.kis.powp.jobs2d.transformation;
 
+/**
+ * Class used for point rotating.
+ */
 public class RotatePointTransformation implements PointTransformation {
     private double angle;
 
+    /**
+     * Creates a RotatePointTransformation.
+     * @param angleInDegrees Angle in degrees by which the point should be rotated.
+     */
     public RotatePointTransformation(double angleInDegrees) {
         angle = Math.toRadians(angleInDegrees);
     }
 
+    /**
+     * Get x coordinate after point rotation.
+     * @param x Current x coordinate.
+     * @param y Current y coordinate.
+     * @return x coordinate for a point after rotating.
+     */
     @Override
     public int getTransformedX(int x, int y) {
         return (int) (x * Math.cos(angle) - y * Math.sin(angle));
     }
 
+    /**
+     * Get y coordinate after point rotation.
+     * @param x Current x coordinate.
+     * @param y Current y coordinate.
+     * @return y coordinate for a point after rotating.
+     */
     @Override
     public int getTransformedY(int x, int y) {
         return (int) (x * Math.sin(angle) + y * Math.cos(angle));
