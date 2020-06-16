92879c174d336b779e4ee7d1a2838a5862e636e7
Mmati1996 <mat.duk.at@gmail.com>
2020-06-16 17:25:54 +0200

    changed TransformationDecorator to Transformation

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/TransformationAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/TransformationAdapter.java
@@ -1,13 +1,13 @@
 package edu.kis.powp.jobs2d.drivers.adapter;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
-import edu.kis.powp.jobs2d.features.DriverFeature;
+import edu.kis.powp.jobs2d.features.Transformations.Transformation;
 import edu.kis.powp.jobs2d.features.Transformations.TransformationDecorator;
 
 public class TransformationAdapter implements Job2dDriver {
     private Job2dDriver driver;
     private String name;
-    private TransformationDecorator transformator;
+    private Transformation transformator;
 
     public TransformationAdapter(Job2dDriver driver, String name) {
         this.driver = driver;
