8d9d5ec5b24a9ca0410dc9fd5ee331f073efe167
Kowalczyk.Bartosz <215772@edu.p.lodz.pl>
2020-06-16 10:00:36 +0200

    automatic refactoring
    Signed-off-by: Kowalczyk.Bartosz <215772@edu.p.lodz.pl>

--- src/main/java/edu/kis/powp/jobs2d/command/manager/parsing/JsonParser.java
+++ src/main/java/edu/kis/powp/jobs2d/command/manager/parsing/JsonParser.java
@@ -8,29 +8,48 @@ import edu.kis.powp.jobs2d.command.SetPositionCommand;
 
 import java.util.ArrayList;
 import java.util.List;
-/**Class responsible for creating
+
+/**
+ * Class responsible for creating
  * command
  * from String in JSON format
- *  and exporting it to .json file*/
+ * and exporting it to .json file
+ */
 public class JsonParser implements Parser {
-	/**Inner class needed to parse JSON */
+	/**
+	 * Inner class needed to parse JSON
+	 */
 	public static class JsonDriverCommand {
-		/**Operation structure*/
+		/**
+		 * Operation structure
+		 */
 		static class operation {
-			/**name of operation*/
+			/**
+			 * name of operation
+			 */
 			String opName;
-			/**X coordinate*/
+			/**
+			 * X coordinate
+			 */
 			int x;
-			/**Y coordinate*/
+			/**
+			 * Y coordinate
+			 */
 			int y;
 		}
-		/**List of operations*/
+
+		/**
+		 * List of operations
+		 */
 		List<operation> operations;
 	}
 
-	/**Creating command from String
+	/**
+	 * Creating command from String
+	 *
 	 * @param commandAsString command in String
-	 * @return command object*/
+	 * @return command object
+	 */
 	@Override
 	public DriverCommand parseFromString(String commandAsString) {
 		Gson gson = new Gson();
@@ -45,9 +64,13 @@ public class JsonParser implements Parser {
 			return driverCommands.get(0);
 		return new CompoundCommand(driverCommands);
 	}
-	/**Creating JSON String from command object
+
+	/**
+	 * Creating JSON String from command object
+	 *
 	 * @param driverCommand command to parse
-	 * @return command in JSON format*/
+	 * @return command in JSON format
+	 */
 	@Override
 	public String parseToString(DriverCommand driverCommand) {
 		Gson gson = new Gson();
