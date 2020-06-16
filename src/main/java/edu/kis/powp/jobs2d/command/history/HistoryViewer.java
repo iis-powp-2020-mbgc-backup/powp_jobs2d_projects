80d12dc68de684bd5d0a662b9bf687915894541e
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-06-16 21:31:49 +0200

    Minor changes v2

--- src/main/java/edu/kis/powp/jobs2d/command/history/HistoryViewer.java
+++ src/main/java/edu/kis/powp/jobs2d/command/history/HistoryViewer.java
@@ -1,25 +1,23 @@
 package edu.kis.powp.jobs2d.command.history;
 
-import java.awt.Dimension;
-import java.awt.HeadlessException;
-
-import javax.swing.DefaultListModel;
-import javax.swing.JFrame;
-import javax.swing.JList;
-
 import edu.kis.powp.appbase.gui.WindowComponent;
 import edu.kis.powp.jobs2d.command.manager.DriverCommandManager;
 import edu.kis.powp.jobs2d.events.SelectHistoryListOptionListener;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class HistoryViewer extends JFrame implements WindowComponent {
 
     private static final long serialVersionUID = 1L;
-    private JList<HistoryEntry> historyCommandList;
-    private DriverCommandManager driverCommandManager;
+    private final JList<HistoryEntry> historyCommandList;
+    private final DefaultListModel<HistoryEntry> historyEntryDefaultListModel;
+    private final DriverCommandManager driverCommandManager;
 
     public HistoryViewer(DefaultListModel<HistoryEntry> historyCommandList, DriverCommandManager driverCommandManager)
             throws HeadlessException {
-        this.historyCommandList = new JList<HistoryEntry>(historyCommandList);
+        this.historyCommandList = new JList<>(historyCommandList);
+        this.historyEntryDefaultListModel = historyCommandList;
         this.driverCommandManager = driverCommandManager;
         setupWindow();
     }
@@ -29,7 +27,7 @@ public class HistoryViewer extends JFrame implements WindowComponent {
         this.setTitle("History");
         this.add(historyCommandList);
         historyCommandList.addListSelectionListener(
-                new SelectHistoryListOptionListener(driverCommandManager, historyCommandList));
+                new SelectHistoryListOptionListener(driverCommandManager, historyEntryDefaultListModel));
     }
 
     @Override
--- src/main/java/edu/kis/powp/jobs2d/events/SelectHistoryListOptionListener.java
+++ src/main/java/edu/kis/powp/jobs2d/events/SelectHistoryListOptionListener.java
@@ -1,19 +1,18 @@
 package edu.kis.powp.jobs2d.events;
 
-import javax.swing.JList;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-
 import edu.kis.powp.jobs2d.command.history.HistoryEntry;
 import edu.kis.powp.jobs2d.command.manager.DriverCommandManager;
 
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
 public class SelectHistoryListOptionListener implements ListSelectionListener {
 
-    private DriverCommandManager commandManager;
-    private final JList<HistoryEntry> historyCommandList;
+    private final DefaultListModel<HistoryEntry> historyCommandList;
+    private final DriverCommandManager commandManager;
 
-    public SelectHistoryListOptionListener(DriverCommandManager commandManager,
-            JList<HistoryEntry> historyCommandList) {
+    public SelectHistoryListOptionListener(DriverCommandManager commandManager, DefaultListModel<HistoryEntry> historyCommandList) {
         this.commandManager = commandManager;
         this.historyCommandList = historyCommandList;
     }
@@ -21,9 +20,12 @@ public class SelectHistoryListOptionListener implements ListSelectionListener {
     @Override
     public void valueChanged(ListSelectionEvent e) {
         if (e.getValueIsAdjusting() == false) {
-            HistoryEntry selectedValue = historyCommandList.getSelectedValue();
-            if (selectedValue != null) {
-                commandManager.setCurrentCommand(selectedValue.getCommand());
+            final int selectedIndex = ((JList<HistoryEntry>) e.getSource()).getSelectedIndex();
+            if (selectedIndex != -1) {
+                HistoryEntry selectedValue = historyCommandList.get(selectedIndex);
+                if (selectedValue != null) {
+                    commandManager.setCurrentCommand(selectedValue.getCommand());
+                }
             }
         }
     }
