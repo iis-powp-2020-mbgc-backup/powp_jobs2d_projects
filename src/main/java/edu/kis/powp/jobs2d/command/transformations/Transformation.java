bd935186e9b39390fc249ab56a7b7b33e5fc8a4b
Leonard Wojtczak <medyk170@gmail.com>
2020-06-20 18:21:06 +0200

    Fix buildCommandList method in Transformation class

--- src/main/java/edu/kis/powp/jobs2d/command/transformations/Transformation.java
+++ src/main/java/edu/kis/powp/jobs2d/command/transformations/Transformation.java
@@ -21,9 +21,9 @@ public class Transformation implements ITransformation {
 	public ICompoundCommand transform(CommandCoordinatesVisitor visitor) {
 		
 		List<Line2d> lines = visitor.getAllCommandsCoordinates();
-		
+
 		properTransformation(lines);
-		
+
 		List<DriverCommand> commandList = buildCommandList(lines);
 
 		return new ImmutableComplexCommand(commandList);
@@ -34,7 +34,6 @@ public class Transformation implements ITransformation {
 	}
 
 	protected static void moveToPosition(List<Line2d> lines, int x, int y) {
-		
 		for(int i = 0; i < lines.size(); i++) {
 			lines.get(i).setStartPosX(lines.get(i).getStartPosX() + x);
 			lines.get(i).setStartPosY(lines.get(i).getStartPosY() + y);
@@ -47,26 +46,13 @@ public class Transformation implements ITransformation {
 		
 		List<DriverCommand> commandList = new ArrayList<>();
 
-		if(lines.size() >= 1) {
-			commandList.add(new SetPositionCommand(lines.get(0).getStartPosX(),
-					lines.get(0).getStartPosY()));
-		}
-
-		for(int i = 1; i < lines.size(); i++) {
-			if(Line2d.checkIfConnected(lines.get(i - 1), lines.get(i))) {
-				commandList.add(new OperateToCommand(lines.get(i).getStartPosX(),
-						lines.get(i).getStartPosY()));
+		for (Line2d line : lines) {
+			if (!line.getOperateTo()) {
+				commandList.add(new SetPositionCommand(line.getStartPosX(), line.getStartPosY()));
 			}
 			else {
-				commandList.add(new SetPositionCommand(lines.get(i).getStartPosX(),
-						lines.get(i).getStartPosY()));
+				commandList.add(new OperateToCommand(line.getEndPosX(), line.getEndPosY()));
 			}
-		}
-
-		if(lines.size() >= 1) {
-			Line2d l = lines.get(lines.size()-1);
-			commandList.add(new OperateToCommand(l.getEndPosX(),
-					l.getEndPosY()));
 		}
 
 		return commandList;
