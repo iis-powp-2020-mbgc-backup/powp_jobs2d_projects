31975f6af86ec21329178c1ca94e6e9ced6a6b3a
Adrian Ałaszewski <wns2148810@interia.pl>
2020-06-24 11:16:08 +0200

    Fixed window editor command removed bug

--- src/main/java/edu/kis/powp/jobs2d/command/gui/ComplexCommandEditorWindow.java
+++ src/main/java/edu/kis/powp/jobs2d/command/gui/ComplexCommandEditorWindow.java
@@ -213,15 +213,18 @@ public class ComplexCommandEditorWindow extends JFrame implements WindowComponen
 		}
 	}
 
-
 	public void updateViewToCurrentCommand() {
 		currentCommand = (ICompoundCommand) CommandsFeature.getDriverCommandManager().getCurrentCommand();
 
-		if(currentCommand!= null) {
+		if(currentCommand != null) {
 			updateCommandStatistics(currentCommand);
 			complexCommandEditor = new ComplexCommandEditor(currentCommand);
 			updateJList(complexCommandEditor.getEditedComplexCommand());
 			commandNameValue.setText(CommandsFeature.getDriverCommandManager().getCurrentCommandString());
+		} else {
+			commandLengthValue.setText("N/A");
+			numberOfCommandsValue.setText("N/A");
+			listModel.clear();
 		}
 
 		paramXInput.setText("");
