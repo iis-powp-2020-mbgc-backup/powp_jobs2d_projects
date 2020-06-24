8ce7d57c5e6e54942e917b9f58370411da4f48e0
arekdziuda <arekdziuda@gmail.com>
2020-06-24 21:15:41 +0200

    Redesigned history component to be independent of the GUI

--- src/main/java/edu/kis/powp/jobs2d/command/historyComponent/HistoryFeature.java
+++ src/main/java/edu/kis/powp/jobs2d/command/historyComponent/HistoryFeature.java
@@ -8,12 +8,13 @@ import java.awt.event.ActionEvent;
 public class HistoryFeature {
 
     public static void setupHistoryFeature(Application application) {
-        HistoryManagerWindow history = new HistoryManagerWindow(CommandsFeature.getDriverCommandManager());
+        HistoryManager history = new HistoryManager(CommandsFeature.getDriverCommandManager());
+        HistoryManagerWindow historyWindow = new HistoryManagerWindow(CommandsFeature.getDriverCommandManager());
         HistoryWindowCommandChangeObserver historyObserver = new HistoryWindowCommandChangeObserver(history);
         CommandsFeature.getDriverCommandManager().getChangePublisher().addSubscriber(historyObserver);
 
         application.addComponentMenu(HistoryManagerWindow.class, "History");
-        application.addComponentMenuElement(HistoryManagerWindow.class, "Show History Window", (ActionEvent e) -> history.changeWindowVisibility());
+        application.addComponentMenuElement(HistoryManagerWindow.class, "Show History Window", (ActionEvent e) -> historyWindow.changeWindowVisibility());
         application.addComponentMenuElement(HistoryManagerWindow.class, "Clear History",  (ActionEvent e) -> history.clearHistoryOfCommand());
     }
 }
--- src/main/java/edu/kis/powp/jobs2d/command/historyComponent/HistoryManagerWindow.java
+++ src/main/java/edu/kis/powp/jobs2d/command/historyComponent/HistoryManagerWindow.java
@@ -12,27 +12,20 @@ import java.time.format.DateTimeFormatter;
 public class HistoryManagerWindow extends JFrame implements WindowComponent {
 
     private JList<CommandHistory> entityHistory;
-    private final DefaultListModel<CommandHistory> historyEntryDefaultListModel;
-    private DriverCommandManager commandManager;
     private JFrame frame;
     Dimension screenSize;
-    private static final String HEAD_STRING = "History of commands: \n";
     private static final String JFRAME_TITLE = "Command History";
-    private static final String HISTORY_TIME_PATTERN = "HH:mm:ss";
 
     public void changeWindowVisibility() {
         frame.setVisible(true);
     }
 
     public HistoryManagerWindow(DriverCommandManager commandManager) {
-        DefaultListModel<CommandHistory> entityHistoryNewListModel = new DefaultListModel<>();
-        this.entityHistory = new JList<>(entityHistoryNewListModel);
-        this.historyEntryDefaultListModel = entityHistoryNewListModel;
+        this.entityHistory = new JList<>(HistoryManager.historyEntryDefaultListModel);
         this.add(entityHistory);
         this.entityHistory.addListSelectionListener(
-                new SelectHistoryListOptionListener(commandManager, historyEntryDefaultListModel));
+                new SelectHistoryListOptionListener(commandManager, HistoryManager.historyEntryDefaultListModel));
 
-        this.commandManager = commandManager;
         frame = new JFrame(JFRAME_TITLE);
         entityHistory.setPreferredSize(new Dimension(500, 500));
         screenSize = new Dimension(600, 300);
@@ -41,21 +34,6 @@ public class HistoryManagerWindow extends JFrame implements WindowComponent {
         frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         frame.pack();
         frame.setVisible(false);
-    }
-
-    public void updateCommandListField() {
-        CommandHistory history = new CommandHistory(java.time.LocalTime.now().format(DateTimeFormatter.ofPattern(HISTORY_TIME_PATTERN)),
-                commandManager.getCurrentCommandString(), commandManager.getCurrentCommand());
-        System.out.println(history);
-        System.out.println(commandManager.getCurrentCommandString());
-        System.out.println(commandManager.getCurrentCommandString());
-        System.out.println(commandManager.getCurrentCommand());
-        historyEntryDefaultListModel.addElement(history);
-    }
-
-    public void clearHistoryOfCommand() {
-        if( historyEntryDefaultListModel != null )
-            historyEntryDefaultListModel.clear();
     }
 
     @Override
