56614b0569c4833ec6c1005bd76619723ba3bbc0
Lisek8 <fafik989@gmail.com>
2020-06-30 11:40:56 +0200

    PR Suggestions: constructor divided into seperate methods, replaced reflection when changing coordinates, visitor extracted to seperate class and code now uses tabs instead of spaces.

--- src/main/java/edu/kis/powp/jobs2d/command/gui/CommandEditorWindow.java
+++ src/main/java/edu/kis/powp/jobs2d/command/gui/CommandEditorWindow.java
@@ -3,154 +3,160 @@ package edu.kis.powp.jobs2d.command.gui;
 import edu.kis.powp.appbase.gui.WindowComponent;
 import edu.kis.powp.jobs2d.command.*;
 import edu.kis.powp.jobs2d.command.manager.DriverCommandManager;
+import edu.kis.powp.jobs2d.command.visitor.CommandCoordinateChangerVisitor;
+import edu.kis.powp.jobs2d.command.visitor.CommandListingVisitor;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.util.Collections;
 
-public class CommandEditorWindow extends JFrame implements WindowComponent, CommandVisitorInterface {
+public class CommandEditorWindow extends JFrame implements WindowComponent {
 
-    private static final long serialVersionUID = 9204679248304669949L;
-    private DriverCommandManager commandManager;
-    private JTextArea currentCommandField;
-    private JList<DriverCommand> commandPartsList;
-    private JButton moveUp;
-    private JButton moveDown;
-    private JTextField coordX;
-    private JTextField coordY;
-    private JTextArea coordinatesX;
-    private JTextArea coordinatesY;
-    private JButton changeCoordinates;
-    private DefaultListModel<DriverCommand> listModel;
+	private static final long serialVersionUID = 9204679248304669949L;
+	private DriverCommandManager commandManager;
+	private JTextArea currentCommandField;
+	private JList<DriverCommand> commandPartsList;
+	private JButton moveUp;
+	private JButton moveDown;
+	private JTextField coordX;
+	private JTextField coordY;
+	private JTextArea coordinatesX;
+	private JTextArea coordinatesY;
+	private JButton changeCoordinates;
+	private DefaultListModel<DriverCommand> listModel;
+	private CommandListingVisitor commandListingVisitor;
+	private CommandCoordinateChangerVisitor commandCoordinateChangerVisitor;
 
-    public CommandEditorWindow(DriverCommandManager commandManager) {
-        this.setTitle("Command Editor");
-        this.setMinimumSize(new Dimension(500, 600));
-        Container content = this.getContentPane();
-        content.setLayout(new GridBagLayout());
-        content.setBackground(Color.WHITE);
-        this.commandManager = commandManager;
+	public CommandEditorWindow(DriverCommandManager commandManager) {
+		commandCoordinateChangerVisitor = new CommandCoordinateChangerVisitor();
+		this.setTitle("Command Editor");
+		this.setMinimumSize(new Dimension(500, 600));
+		Container content = this.getContentPane();
+		content.setLayout(new GridBagLayout());
+		content.setBackground(Color.WHITE);
+		this.commandManager = commandManager;
 
-        GridBagConstraints c = new GridBagConstraints();
-        currentCommandField = new JTextArea("");
-        currentCommandField.setEditable(false);
-        c.fill = GridBagConstraints.BOTH;
-        c.gridx = 0;
-        c.gridy = 0;
-        c.weighty = 0.05;
-        c.weightx = 1;
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 0;
+		c.weightx = 1;
 
-        content.add(currentCommandField, c);
-        updateCurrentCommandField();
+		createCurrentCommandField(content, c);
+		createCommandList(content, c);
+		createMoveButtons(content, c);
+		createChangeCoordinatesOptions(content, c);
+	}
 
-        commandPartsList = new JList<>();
-        listModel = new DefaultListModel<>();
-        commandPartsList.setModel(listModel);
-        c.gridy = 1;
-        c.weighty = 0.75;
-        content.add(commandPartsList, c);
-        updateCurrentCommandPartList();
+	public void changeCoordinates(int x, int y, DriverCommand command) {
+		int index = commandPartsList.getSelectedIndex();
+		DriverCommand newCommand = commandCoordinateChangerVisitor.changeCoordinates(listModel.get(index), x, y);
+		listModel.set(index, newCommand);
+		commandManager.setCurrentCommand(Collections.list(listModel.elements()), "Custom command");
+	}
 
-        c.weighty = 0.05;
+	public void moveUp(DriverCommand command) {
+		int index = commandPartsList.getSelectedIndex();
+		if (index > 0) {
+			listModel.set(index, listModel.get(index - 1));
+			listModel.set(index - 1, command);
+		}
+		commandManager.setCurrentCommand(Collections.list(listModel.elements()), "Custom command");
+		if (index > 0)
+			commandPartsList.setSelectedIndex(index - 1);
+	}
 
-        c.gridy = 2;
-        moveUp = new JButton("Move command higher ");
-        moveUp.addActionListener((ActionEvent e) -> this.moveUp(commandPartsList.getSelectedValue()));
-        moveUp.setPreferredSize(new Dimension(300, 30));
-        content.add(moveUp, c);
+	public void moveDown(DriverCommand command) {
+		int index = commandPartsList.getSelectedIndex();
+		if (index < listModel.getSize() - 1) {
+			listModel.set(index, listModel.get(index + 1));
+			listModel.set(index + 1, command);
+		}
+		commandManager.setCurrentCommand(Collections.list(listModel.elements()), "Custom command");
+		if (index < listModel.getSize() - 1)
+			commandPartsList.setSelectedIndex(index + 1);
+	}
 
-        c.gridy = 3;
-        moveDown = new JButton("Move command lower ");
-        moveDown.setPreferredSize(new Dimension(300, 30));
-        moveDown.addActionListener((ActionEvent e) -> this.moveDown(commandPartsList.getSelectedValue()));
-        content.add(moveDown, c);
+	@Override
+	public void HideIfVisibleAndShowIfHidden() {
+		this.setVisible(!this.isVisible());
+	}
 
-        c.gridy = 4;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        Container container = new Container();
-        GridLayout gridLayout = new GridLayout(1, 4);
-        gridLayout.setHgap(25);
-        container.setLayout(gridLayout);
+	public void updateCurrentCommandField() {
+		currentCommandField.setText(commandManager.getCurrentCommandString());
+	}
 
-        coordinatesX = new JTextArea("X coordinate ");
-        coordinatesX.setEditable(false);
-        container.add(coordinatesX);
-        coordX = new JTextField("0");
-        container.add(coordX);
+	public void updateCurrentCommandPartList() {
+		DriverCommand command = commandManager.getCurrentCommand();
+		listModel.removeAllElements();
+		if (command != null) {
+			command.accept(commandListingVisitor);
+		}
+	}
 
-        coordinatesY = new JTextArea("Y coordinate ");
-        coordinatesY.setEditable(false);
-        container.add(coordinatesY);
-        coordY = new JTextField("0");
-        container.add(coordY);
-        content.add(container, c);
+	private void createCurrentCommandField(Container content, GridBagConstraints c) {
+		c.gridy = 0;
+		c.weighty = 0.05;
+		currentCommandField = new JTextArea("");
+		currentCommandField.setEditable(false);
+		content.add(currentCommandField, c);
+		updateCurrentCommandField();
+	}
 
-        c.gridy = 5;
-        c.fill = GridBagConstraints.BOTH;
-        changeCoordinates = new JButton("Change coordinates ");
-        changeCoordinates.setPreferredSize(new Dimension(300, 30));
-        changeCoordinates.addActionListener(
-                (ActionEvent e) -> this.changeCoordinates(Integer.parseInt(coordX.getText()), Integer.parseInt(coordY.getText()),
-                        commandPartsList.getSelectedValue()));
-        content.add(changeCoordinates, c);
-    }
+	private void createCommandList(Container content, GridBagConstraints c) {
+		c.gridy = 1;
+		c.weighty = 0.75;
+		commandPartsList = new JList<>();
+		listModel = new DefaultListModel<>();
+		commandPartsList.setModel(listModel);
+		commandListingVisitor = new CommandListingVisitor(listModel);
+		content.add(commandPartsList, c);
+		updateCurrentCommandPartList();
+	}
 
-    public void changeCoordinates(int x, int y, DriverCommand command) {
-        int index = commandPartsList.getSelectedIndex();
-        try {
-            listModel.set(index, command.getClass().getDeclaredConstructor(int.class, int.class).newInstance(x, y));
-        } catch (ReflectiveOperationException ignored) { }
-        commandManager.setCurrentCommand(Collections.list(listModel.elements()), "Custom command");
-    }
+	private void createMoveButtons(Container content, GridBagConstraints c) {
+		c.weighty = 0.05;
+		c.gridy = 2;
+		moveUp = new JButton("Move command higher ");
+		moveUp.addActionListener((ActionEvent e) -> this.moveUp(commandPartsList.getSelectedValue()));
+		moveUp.setPreferredSize(new Dimension(300, 30));
+		content.add(moveUp, c);
 
-    public void moveUp(DriverCommand command) {
-        int index = commandPartsList.getSelectedIndex();
-        if (index > 0) {
-            listModel.set(index, listModel.get(index - 1));
-            listModel.set(index - 1, command);
-        }
-        commandManager.setCurrentCommand(Collections.list(listModel.elements()), "Custom command");
-        if(index > 0)
-            commandPartsList.setSelectedIndex(index - 1);
-    }
+		c.gridy = 3;
+		moveDown = new JButton("Move command lower ");
+		moveDown.setPreferredSize(new Dimension(300, 30));
+		moveDown.addActionListener((ActionEvent e) -> this.moveDown(commandPartsList.getSelectedValue()));
+		content.add(moveDown, c);
+	}
 
-    public void moveDown(DriverCommand command) {
-        int index = commandPartsList.getSelectedIndex();
-        if (index < listModel.getSize() - 1) {
-            listModel.set(index, listModel.get(index + 1));
-            listModel.set(index + 1, command);
-        }
-        commandManager.setCurrentCommand(Collections.list(listModel.elements()), "Custom command");
-        if(index < listModel.getSize() - 1)
-            commandPartsList.setSelectedIndex(index + 1);
-    }
+	private void createChangeCoordinatesOptions(Container content, GridBagConstraints c) {
+		c.gridy = 4;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		Container container = new Container();
+		GridLayout gridLayout = new GridLayout(1, 4);
+		gridLayout.setHgap(25);
+		container.setLayout(gridLayout);
 
-    @Override public void HideIfVisibleAndShowIfHidden() {
-        this.setVisible(!this.isVisible());
-    }
+		coordinatesX = new JTextArea("X coordinate ");
+		coordinatesX.setEditable(false);
+		container.add(coordinatesX);
+		coordX = new JTextField("0");
+		container.add(coordX);
 
-    public void updateCurrentCommandField() {
-        currentCommandField.setText(commandManager.getCurrentCommandString());
-    }
+		coordinatesY = new JTextArea("Y coordinate ");
+		coordinatesY.setEditable(false);
+		container.add(coordinatesY);
+		coordY = new JTextField("0");
+		container.add(coordY);
+		content.add(container, c);
 
-    public void updateCurrentCommandPartList() {
-        DriverCommand command = commandManager.getCurrentCommand();
-        listModel.removeAllElements();
-        if (command != null) {
-            command.accept(this);
-        }
-    }
-
-    @Override public void visit(OperateToCommand driver) {
-        listModel.addElement(driver);
-    }
-
-    @Override public void visit(SetPositionCommand driver) {
-        listModel.addElement(driver);
-    }
-
-    @Override public void visit(ICompoundCommand driver) {
-        driver.iterator().forEachRemaining(command -> command.accept(this));
-    }
+		c.gridy = 5;
+		c.fill = GridBagConstraints.BOTH;
+		changeCoordinates = new JButton("Change coordinates ");
+		changeCoordinates.setPreferredSize(new Dimension(300, 30));
+		changeCoordinates.addActionListener(
+				(ActionEvent e) -> this.changeCoordinates(Integer.parseInt(coordX.getText()), Integer.parseInt(coordY.getText()),
+						commandPartsList.getSelectedValue()));
+		content.add(changeCoordinates, c);
+	}
 }
---
+++ src/main/java/edu/kis/powp/jobs2d/command/visitor/CommandListingVisitor.java
@@ -0,0 +1,28 @@
+package edu.kis.powp.jobs2d.command.visitor;
+
+import edu.kis.powp.jobs2d.command.*;
+
+import javax.swing.*;
+
+public class CommandListingVisitor implements CommandVisitorInterface {
+	private DefaultListModel<DriverCommand> listModel;
+
+	public CommandListingVisitor(DefaultListModel<DriverCommand> listModel) {
+		this.listModel = listModel;
+	}
+
+	@Override
+	public void visit(OperateToCommand driver) {
+		listModel.addElement(driver);
+	}
+
+	@Override
+	public void visit(SetPositionCommand driver) {
+		listModel.addElement(driver);
+	}
+
+	@Override
+	public void visit(ICompoundCommand driver) {
+		driver.iterator().forEachRemaining(command -> command.accept(this));
+	}
+}
